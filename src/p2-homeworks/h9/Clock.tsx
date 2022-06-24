dbe9383ed9ac2423a40dc44ac7bbf6ff13818106
Bespalov Dmitry <dev.bespalov@gmail.com>
2022-06-24 17:15:41 +0800

    HW9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,49 +1,58 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import paperStyle from '../../common/styles/Paper.module.css'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearTimeout(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
+    const leadingZero = (num: number) => `0${num}`.slice(-2);
+    const formatTime = (date: Date) =>
+        [date.getHours(), date.getMinutes(), date.getSeconds()]
+            .map(leadingZero)
+            .join(':');
+    let correctMonth = date.getMonth() + 1
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
-
+    const stringTime = formatTime(date)
+    const stringDate = `${(date.getDate() < 10 ? '0' : '') + date.getDate()}/${(correctMonth < 10 ? '0' : '') + correctMonth}/${date.getFullYear()}`
     return (
         <div>
-            <div
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-            >
-                {stringTime}
+            <div className={paperStyle.paper} style={{height: '40px', width: '125px'}}>
+                <div
+                    style={{fontSize: '20px', fontWeight: 'bold'}}
+                    onMouseEnter={onMouseEnter}
+                    onMouseLeave={onMouseLeave}
+                >
+                    {stringTime}
+                </div>
+
+                {show && (
+                    <div style={{fontSize: '20px', fontWeight: 'bold'}}>
+                        {stringDate}
+                    </div>
+                )}
             </div>
 
-            {show && (
-                <div>
-                    {stringDate}
-                </div>
-            )}
-
-            <SuperButton onClick={start}>start</SuperButton>
-            <SuperButton onClick={stop}>stop</SuperButton>
+            <SuperButton def onClick={start}>start</SuperButton>
+            <SuperButton red onClick={stop}>stop</SuperButton>
 
         </div>
     )
